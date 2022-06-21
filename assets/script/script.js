ade4ea652a6b1aa17520512682597705da35e098
rrayachhetri <rrayachhetri@gmail.com>
2022-06-21 15:15:11 -0700

    loading..

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,11 @@
- function namE(paraA, paraB) {
-    var someOne = paraA + "loves" + paraB;
-    return someOne;
- }
- namE(ram, Sita)
+//  function namE(paraA, paraB) {
+//     var someOne = paraA + "loves" + paraB;
+//     return someOne;
+//  }
+//  namE(ram, Sita)
+var btnAdd = document.querySelector("#addBtn");
+
+btnAdd.addEventListener("click", function(){
+   window.alert("windows pop-up!");
+   // localStorage.setItem("key", JSON.stringify(tasks));
+});
